09eb88d08c507c2e79482be6440a013dd7d88674
Mario Casciaro <mariocasciaro@gmail.com>
2013-11-11 19:39:36 +0000

    add isRelative option Remove duplicate files

--- tasks/particleslinker.js
+++ tasks/particleslinker.js
@@ -9,6 +9,7 @@
 'use strict';
 
 var util = require('util'),
+  _ = require('lodash'),
   path = require('path');
 
 function isUrl(url) {
@@ -20,7 +21,7 @@ module.exports = function(grunt) {
 
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
-
+  
 	grunt.registerMultiTask('particles-linker', 'Your task description goes here.', function() {
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
@@ -28,7 +29,8 @@ module.exports = function(grunt) {
 			endTag: '<!--SCRIPTS END-->',
 			fileTmpl: '<script src="%s"></script>',
 			appRoot: '',
-      publicUrlRoot: ''
+      publicUrlRoot: '',
+      isRelative: false
 		});
 
 
@@ -46,33 +48,34 @@ module.exports = function(grunt) {
           scripts.push(filepath);
         } else {
           var expandedFiles = grunt.file.expand({}, filepath);
-          expandedFiles.forEach(function(expandedFile) {
-            if (grunt.file.exists(expandedFile)) {
-              scripts.push(expandedFile);
-            } else {
-              grunt.log.error('Source file "' + expandedFile + '" not found.');
-            }
-          });
+          Array.prototype.push.apply(scripts, expandedFiles);
+          scripts = _.unique(scripts);
         }
 			});
       
-      scripts = scripts.map(function (filepath) {
-        var ret;
-        if(isUrl(filepath)) {
-          ret = util.format(options.fileTmpl, filepath);
-        } else {
-          ret = util.format(options.fileTmpl, path.join("/", options.publicUrlRoot, filepath.replace(options.appRoot, '')));
-        }
-          
-        grunt.log.writeln('Injecting ' + ret);
-        return ret;
-			});
+      if(!options.isRelative) {
+        //pre-apply the template only if it's not relative
+        
+        scripts = scripts.map(function (filepath) {
+          var ret;
+          if(isUrl(filepath)) {
+            ret = util.format(options.fileTmpl, filepath);
+          } else {
+            ret = util.format(options.fileTmpl, 
+              path.join("/", options.publicUrlRoot, filepath.replace(options.appRoot, '')));
+          }
+
+          grunt.log.writeln('Preparing to inject ' + ret);
+          return ret;
+        });
+      }
+      
 
 			grunt.file.expand({}, f.dest).forEach(function(dest){
 				page = grunt.file.read(dest);
 				start = page.indexOf(options.startTag);
 
-				end = page.indexOf(options.endTag);
+				end = page.indexOf(options.endTag, start);
 				if (start === -1 || end === -1 || start >= end) {
 					return;
 				} else {
@@ -83,8 +86,14 @@ module.exports = function(grunt) {
             padding += page.charAt(ind);
             ind -= 1;
           }
+          var newScripts = scripts;
+          if(options.isRelative) {
+            newScripts = scripts.map(function(script) {
+              return util.format(options.fileTmpl, path.relative(path.dirname(dest), script));
+            });
+          }
           console.log('padding length', padding.length);
-					newPage = page.substr(0, start + options.startTag.length)+'\n' + padding + scripts.join('\n'+padding) + '\n' + padding + page.substr(end);
+					newPage = page.substr(0, start + options.startTag.length)+'\n' + padding + newScripts.join('\n'+padding) + '\n' + padding + page.substr(end);
 					// Insert the scripts
 					grunt.file.write(dest, newPage);
 					grunt.log.writeln('File "' + dest + '" updated.');
@@ -92,5 +101,4 @@ module.exports = function(grunt) {
 			});
 		});
 	});
-
 };
